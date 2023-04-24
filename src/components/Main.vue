33ad2a5116023b54cbdfe7f20548cf0c76474d0e
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-24 08:49:21 -0300

    Inserindo e estilizando o elemento Rightside

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,28 @@
 <template>
     <main class="main">
         <LeftSide/>
+        <RightSide/>
     </main>
 </template>
 
 <script>
 import LeftSide from './LeftSide.vue';
-//import RightSide from './RightSide.vue';
+import RightSide from './RightSide.vue';
 export default {
     name: "MainComponent",
     components: {
-        LeftSide, //RightSide
+        LeftSide, RightSide
     }
 }
 </script>
 
 <style>
-.main{
-    display: flex;
-    justify-content: center;
-    gap: 64px;
-    margin-top: 6rem;
+@media screen and (min-width: 376px) {
+    .main{
+        display: flex;
+        justify-content: center;
+        gap: 64px;
+        margin-top: 6rem;
+    }
 }
 </style>
--- src/components/RightSide.vue
+++ src/components/RightSide.vue
@@ -5,3 +5,86 @@
         <button class="description__button">Register</button>
     </section>
 </template>
+
+<script>
+export default {
+    name: "Rightside"
+}
+</script>
+
+<style>
+.description{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.description__title{
+    font-size: 24px;
+    font-weight: 600;
+    color: #fff;
+    margin-top: 3.75rem;
+    width: 17.5rem;
+    line-height: 36px;
+}
+
+.description__text{
+    margin-top: 1rem;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+    color: #fff;
+    width: 19rem;
+    line-height: 24px;
+}
+
+.description__button{
+    margin-top: 1.5rem;
+    width: 12.5rem;
+    height: 2.5rem;
+    background-color: white;
+    color: #684BB1;
+    font-weight: 400;
+    line-height: 18px;
+    font-size: 12px;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+}
+
+.description__button:hover{
+    background-color: #E880E8;
+    transition: 200ms;
+}
+
+@media screen and (min-width: 376px) {
+    .description{
+        align-items:start;
+        text-align: left;
+        margin-top: 2rem;
+        line-height: 60px;
+    }
+    
+    .description__title{
+        margin-top: 0.5rem;
+        width: 30.5rem;
+        font-size: 40px;
+        line-height: 60px;
+    }
+    
+    .description__text{
+        margin-top: 1.5rem;
+        width: 32.5rem;
+        font-size: 18px;
+        line-height: 27px;
+    }
+    
+    .description__button{
+        margin-top: 1.5rem;
+        height: 3.5rem;
+        border-radius: 28px;
+        font-size: 18px;
+        box-shadow: 2px 4px 9px 3px rgba(0, 0, 0, 0.258633);
+    }
+}
+</style>
